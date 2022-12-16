1fa96b9517b34137c448054fb16140647066fef6
Martín Baudino <martinbaudino@gmail.com>
2022-12-15 21:32:30 -0300

    Add unnecesary f-string

--- demo_program.py
+++ demo_program.py
@@ -7,10 +7,16 @@ import numpy as np  # Another F401
 
 # Generate E303 too many blank lines
 if __name__ == "__main__":
-    really_long_variable_name_to_reach_the_end_of_the_line = sentence_wrong_place.capitalize().strip().lower().join("another string")
+    really_long_variable_name_to_reach_the_end_of_the_line = (
+        sentence_wrong_place.capitalize().strip().lower().join("another string")
+    )
 
-    print(really_long_variable_name_to_reach_the_end_of_the_line)  # The previous was a very long line, this one too but let's see what happens
+    print(
+        really_long_variable_name_to_reach_the_end_of_the_line
+    )  # The previous was a very long line, this one too but let's see what happens
 
     unused_var = 37
 
     print("Some random text")
+
+    print(f"Some other text with unnecessary f-string")
