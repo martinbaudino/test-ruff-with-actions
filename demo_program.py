beabfab3af4b36fe9dd6f99d01c1ea42da142bcd
Martín Baudino <martinbaudino@gmail.com>
2022-12-15 21:49:22 -0300

    Manual modifications to pass the tests

--- demo_program.py
+++ demo_program.py
@@ -1,16 +1,20 @@
-import pandas as pd  # Unused import to generate F401
+# Uncomment to generate error
+# import pandas as pd  # Unused import to generate F401
 
 sentence_wrong_place = "It should be caught by ruff as E402"
 
-import numpy as np  # Another F401
+# Uncomment to generate error
+# import numpy as np  # Another F401
 
 
-# Generate E303 too many blank lines
+# Add more lines to generate E303 too many blank lines
 if __name__ == "__main__":
+    # Put everything in one line to generate error
     really_long_variable_name_to_reach_the_end_of_the_line = (
         sentence_wrong_place.capitalize().strip().lower().join("another string")
     )
 
+    # Put everything in one line to generate error
     print(
         really_long_variable_name_to_reach_the_end_of_the_line
     )  # The previous was a very long line, this one too but let's see what happens
@@ -18,5 +22,5 @@ if __name__ == "__main__":
     unused_var = 37
 
     print("Some random text")
-
-    print(f"Some other text with unnecessary f-string")
+    # Uncomment to generate error
+    # print(f"Some other text with unnecessary f-string")
